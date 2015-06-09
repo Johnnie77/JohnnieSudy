1141b6941cdd717263d1268c1911ede3e908ba4a
johney <johney@kakao.com>
2015-06-09 18:50:17 +0900

    로그인한 사용자 이름 얻어내기 됨.

--- springgroovy/src/main/groovy/com/pepperclab/springgroovy/HelloController.groovy
+++ springgroovy/src/main/groovy/com/pepperclab/springgroovy/HelloController.groovy
@@ -1,5 +1,7 @@
 package com.pepperclab.springgroovy
 
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
@@ -27,7 +29,9 @@ class HelloController {
 
 	@RequestMapping("/hello")
 	public def hello(Model model) {
-		model.addAttribute("message", "Hello Johnnie")
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String name = auth.getName(); //get logged in username
+		model.addAttribute("message", "Hello ${name}")
 
 		return "hello"
 	}
